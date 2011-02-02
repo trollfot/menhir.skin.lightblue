c8832c84982f6bad49e1e828e22508a21c08b140
Souheil CHELFOUH <trollfot@gmail.com>
2011-02-02 15:46:12 +0100

    Using chameleon templates.

--- src/menhir/skin/lightblue/pages.py
+++ src/menhir/skin/lightblue/pages.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from dolmen.forms.crud import ApplicationForm
+from dolmen.app.layout import master, DefaultView
 from megrok import pagetemplate as pt
 from menhir.skin.lightblue import ILightblueLayer
+
+
+class BlueMaster(master.Master):
+    pt.layer(ILightblueLayer)
 
 
 class DolmenFormTemplate(pt.PageTemplate):
     """Template for a layout aware form.
     """
     pt.view(ApplicationForm)
+    pt.layer(ILightblueLayer)
+
+
+class DolmenViewFormTemplate(pt.PageTemplate):
+    """Template for a layout aware form.
+    """
+    pt.view(DefaultView)
     pt.layer(ILightblueLayer)
